da9c67b8011168bfdec3fb15be5c59c82eaec87a
Guilherme Mergulhão <golimpiof@gmail.com>
2024-10-03 15:34:50 -0300

    refatoração e novas funcionalidades

--- js/app.js
+++ js/app.js
@@ -1,19 +1,40 @@
-// Na aula, essas variáveis foram definidas dentro da função. Não sei dizer o que é melhor ou pior. Talvez faça sentido dentro da função, pois dessa forma
-// o script só roda quando a função é acionada, e não no carregamento da página
-let gameId = document.getElementById(parseInt('id')); // Aqui acabei usando o parseInt, mas na aula foi concatenado no id (`game-${id}`), o que acho que fica melhor
-let botao = document.getElementsByTagName('a'); // Aqui usaram o querySelector pra pegar pela classe o botão. Facilita pois a variável não se torna um array
-let imagem = document.getElementsByClassName('dashboard__item__img'); // Idem acima
+// Refatoração pós resultado na aula. Mantive algumas coisas como estavam, mas algumas precisei mudar para implementar novas funcionalidades sugeridas em desafios
+function alterarStatus(id) {
 
-function alterarStatus(gameId) {
+    // Trouxe as variáveis pra dentro da função, o que fazia mais sentido
+    let gameClicado = document.getElementById(`game-${id}`); // Mudei a forma com que pegava o ID do jogo, separando a variável que representa o jogo que foi clicado
+    let botao = document.getElementsByTagName('a');
+    let imagem = document.getElementsByClassName('dashboard__item__img');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name').textContent; // Adicionei a variável nomeJogo para poder exibir na confirmação
 
-    // A lógica do if ficou igual, a diferença foi que usaram a variável imagem na condição, e as variáveis não são arrays, por conta da diferença na coleta das variáveis
-    if(botao[gameId - 1].classList.contains('dashboard__item__button--return')) { 
-        botao[gameId - 1].classList.remove('dashboard__item__button--return');
-        botao[gameId - 1].textContent = 'Alugar';
-        imagem[gameId - 1].classList.remove('dashboard__item__img--rented');
+    if(botao[id - 1].classList.contains('dashboard__item__button--return')) { 
+
+        // Esse novo if chama a janela de confirmação de devolução
+        if(confirm(`Tem certeza que gostaria de devolver ${nomeJogo}?`)) { 
+            botao[id - 1].classList.remove('dashboard__item__button--return');
+            botao[id - 1].textContent = 'Alugar';
+            imagem[id - 1].classList.remove('dashboard__item__img--rented');
+            quantidadeAlugados(); // Aqui chamo a função de quantidade de jogos alugados sempre que um novo jogo é devolvido
+        }
     } else {
-        botao[gameId - 1].classList.add('dashboard__item__button--return')
-        botao[gameId - 1].textContent = 'Devolver';
-        imagem[gameId - 1].classList.add('dashboard__item__img--rented');
+
+        // Esse novo if chama a janela de confirmação de locação (no desafio era só para devolução, mas achei que ficaria legal)
+        if(confirm(`Tem certeza que gostaria de alugar ${nomeJogo}?`)) { 
+            botao[id - 1].classList.add('dashboard__item__button--return')
+            botao[id - 1].textContent = 'Devolver';
+            imagem[id - 1].classList.add('dashboard__item__img--rented');
+            quantidadeAlugados(); // Aqui chamo a função de quantidade de jogos alugados sempre que um novo jogo é alugado
+        }
     }
 }
+
+// Função para mostrar quantos jogos estão alugados
+function quantidadeAlugados() {
+    let quantidadeAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    console.log(`Jogos alugados no momento: ${quantidadeAlugados}`);
+}
+
+quantidadeAlugados(); // Aqui chamo a função quando a página carrega
+
+// Na resolção do desafio da quantidade de alugados, adicionaram um document.addEventListener
+// Acredito que da forma que fiz ficou mais simples (nem conheço sobre eventListener ainda hahaha), mas não sei se é a melhor abordagem
